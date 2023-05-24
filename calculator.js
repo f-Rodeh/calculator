e815ce794ac523d63c916b3be0bee76a58ac8681
f-Rodeh <rodeh.dev@gmail.com>
2023-05-24 12:56:18 -0600

    Feature assign operators and operands on user input

--- calculator.js
+++ calculator.js
@@ -3,17 +3,23 @@ const OPERATORS = ['+','-','*','/']
 
 window.addEventListener('keypress', e => {
   if ( e.key.match(/[0-9]/) ) {
-    console.log(e)
+    userInputNumber( e.key )
+    console.log({num1, operator, num2})
   } else if ( OPERATORS.includes(e.key) ) {
-    console.log(e)
+    userInputOperator( e.key )
+    console.log({num1, operator, num2})
   }
 })
 
 window.addEventListener('keydown', e => {
   if (e.key === 'Backspace') {
-    console.log(e)
+    userInputBackspace()
+    console.log({num1, operator, num2})
   } else if (e.key === 'Enter') {
     console.log(e)
+  } else if (e.key === 'c'){
+    clear();
+    console.log({num1, operator, num2})
   }
 })
 
@@ -25,12 +31,46 @@ buttons.forEach( button => {
   })
 })
 
-// create str variable operator
-// create str variable num1
-// create str variable num2
-// if user input is a number: 
-  // if there's no operator, concat it to the num1 variable
-  // if there is an operator, concat it to the num2 variable
-// if user input is a valid operator: store it in operator variable
+let operator = '';
+let num1 = '';
+let num2 = '';
+
+function userInputNumber( numStr ){
+  if( !operator ){  
+    num1 += numStr; // concat to num1
+  } else {
+    num2 += numStr; // concat to num2
+  }
+}
+
+function userInputOperator( str ) {
+  if ( !num1 ) num1 = '0';
+  operator = str;
+}
+
+function userInputBackspace(){
+
+  if( num1 && !operator){
+    num1 = erase( num1 )
+  } else if ( operator && !num2 ){
+    operator = erase( operator )
+    if( num1 === '0' ) num1 = ''
+  } else if( num2 ){
+    num2 = erase( num2 )
+  }
+
+  function erase( str ){
+    let array = str.split('');
+    array[ array.length - 1 ] = '';
+    return array.join('')
+  }
+}
+
+function clear(){
+  operator = '';
+  num1 = '';
+  num2 = '';
+}
+
 // create function operate() that defines the operator, uses the function
   // for that operator and displays the result
